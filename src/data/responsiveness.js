afeac0f4cb23234aacb6a4b0c9c72b3532feb79c
ASHUTOSH SINGH <ashutoshsinghvns95@gmail.com>
2023-07-18 05:01:14 +0530

    responsiveness

--- src/data/responsiveness.js
+++ src/data/responsiveness.js
@@ -11,27 +11,27 @@ export const responsive = {
         slidesToSlide: 5
     },
     tablet: {
-        breakpoint: { max: 1323, min: 700 },
+        breakpoint: { max: 1323, min: 998 },
         items: 4,
         slidesToSlide: 2
     },
     tablet2: {
         breakpoint: { max: 998, min: 800 },
         items: 3,
-        slidesToSlide: 3
+        slidesToSlide: 2
     },
     mobile: {
         breakpoint: { max: 800, min: 600 },
         items: 3,
-        slidesToSlide: 3
+        slidesToSlide: 2
     },
     superSmallMobile: {
-        breakpoint: { max: 600, min: 400 },
+        breakpoint: { max: 600, min: 370 },
         items: 2,
         slidesToSlide: 2
     },
     superDuperSmallMobile: {
-        breakpoint: { max: 250, min: 0 },
+        breakpoint: { max: 370, min: 0 },
         items: 1,
         slidesToSlide: 1
     }
